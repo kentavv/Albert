5234b301137d2b5b670231aad5e4e723f174ba91
Kent A. Vander Velden <kent.vandervelden@gmail.com>
2014-02-20 17:03:27 -0600

    Removed dense matrix code.

--- ReduceMatrix.h
+++
@@ -1,9 +0,0 @@
-#ifndef _REDUCE_MATRIX_H_
-#define _REDUCE_MATRIX_H_
-
-#include "Build_defs.h"
-#include "CreateMatrix.h"
-
-int ReduceTheMatrix(Matrix Mptr, int Rows, int Cols, int *Rank);
-
-#endif
