c7e0e4ff46ef0a1484acc437ab2e85befdb2f99b
Kent VanderVelden <kent.vandervelden@gmail.com>
2020-12-19 10:04:28 -0600

    Save and restore Type Table, clean up

--- Type_table.cpp
+++ Type_table.cpp
@@ -41,9 +41,11 @@ using std::vector;
 
 static void EnterBeginBasis(int TTindex, Basis basis);
 static void InitStoreblocksizes();
-static void FillTypecount(int Cur_scan_pos);
-static int InitTypetable();
+
+static void FillTypecount(vector<int> &Type_count, int Cur_scan_pos);
+static int InitTypetable(const vector<int> &Type_count);
 static int FillTypetable(int Cur_scan_pos, vector<int> &Temp_dttt_index);
+
 static int GetIndex(Type Pntr);
 static void PrintType(Type Pntr, FILE *filePtr);
 #if 0
@@ -54,12 +56,13 @@ static void PrintTypetableindex();
 static Type Target_type = nullptr;             /* Input from higher level module. */
 static int Target_type_len = 0;                /* Computed from Target_type.      */
 static int Target_type_deg = 0;                /* Computed from Target_type.      */
-static int *Type_count = nullptr;              /* Used to fill Type_table.        */
-static TT_node *Type_table = nullptr;          /* Heart of the matter.            */
-static int *Type_table_index = nullptr;        /* Map type to Type_table.         */
+static vector<TT_node> Type_table;             /* Heart of the matter.            */
+static vector<int> Type_table_index;           /* Map type to Type_table.         */
 static int Tot_subtypes = 0;                   /* Computed from Type_count. Size of Type_table. */
-static int *Deg_to_type_table_index = nullptr; /* Map Degree to Type_table.*/
+static vector<int> Deg_to_type_table_index;    /* Map Degree to Type_table.*/
 static vector<int> Store_block_sizes;          /* To find offset into Type_table. */
+
+static vector<Degree> last_type;
 
 /*******************************************************************/
 /* GLOBALS INITIALIZED:                                            */
@@ -87,38 +90,26 @@ int CreateTypeTable(Type Cur_type)
     int i = 0;
     
     Target_type_len = 0;
-    while (Cur_type[i++] != 0)
+    while (Cur_type[i++] != 0) {
         Target_type_len++;
+    }
 
-    Target_type = GetNewType(); 
-    assert_not_null(Target_type);
+    last_type = vector<Degree>(Cur_type, Cur_type + Target_type_len);
 
-    for (i=0;i<Target_type_len;i++)
+    Target_type = GetNewType();
+
+    for (i = 0; i < Target_type_len; i++) {
         Target_type[i] = Cur_type[i];
+    }
 
     Target_type_deg = GetDegree(Target_type); 
 
     InitStoreblocksizes();
 
-    Type_count = (int *) (Mymalloc((Target_type_deg + 1) * sizeof(int)));
-    assert_not_null(Type_count);
+    vector<int> Type_count(Target_type_deg + 1, 0);
+    FillTypecount(Type_count, 0);
 
-    for (i = 0; i <= Target_type_deg; i++) {
-        Type_count[i] = 0;
-    }
-    
-    FillTypecount(0);
-
-    if (InitTypetable() != OK) {
-        return 0;
-    }
-
-    if(Type_count) {
-      free(Type_count);
-      Type_count = nullptr;
-    }
-
-    return OK;
+    return InitTypetable(Type_count);
 }
 
 int GetTargetLen()
@@ -129,13 +120,10 @@ int GetTargetLen()
 
 Type GetNewType()
 {
-    Type temp_type;
-    int i;
-
-    temp_type = (Type) (Mymalloc((Target_type_len + 1) * sizeof(Degree))); 
+    Type temp_type = (Type) Mymalloc((Target_type_len + 1) * sizeof(Degree));
     assert_not_null(temp_type);
 
-    for (i=0;i<=Target_type_len;i++) {
+    for (int i = 0; i <= Target_type_len; i++) {
         temp_type[i] = 0;
     }
 
@@ -147,7 +135,7 @@ void NameToType(Name N, Type T)
 {
     assert_not_null_nv(T);
 
-    for (int i=0;i<Target_type_len;i++) {
+    for (int i = 0; i < Target_type_len; i++) {
         T[i] = Type_table[N].type[i];
     }
 }
@@ -155,13 +143,9 @@ void NameToType(Name N, Type T)
 
 void SubtractTypeName(Name n1, Name n2, Name *res_name)
 {
-    Type temp_type;
-    int i;
+    Type temp_type = GetNewType();
 
-    temp_type = GetNewType();
-    assert_not_null_nv(temp_type);
-
-    for (i=0;i<Target_type_len;i++) {
+    for (int i = 0; i < Target_type_len; i++) {
         temp_type[i] = Type_table[n1].type[i] - Type_table[n2].type[i];
     }
 
@@ -173,11 +157,10 @@ void SubtractTypeName(Name n1, Name n2, Name *res_name)
 
 int GetDegree(Type Pntr)
 {
-    int i, deg = 0;
-
     assert_not_null(Pntr);
 
-    for (i = 0; i < Target_type_len; i++) {
+    int deg = 0;
+    for (int i = 0; i < Target_type_len; i++) {
         deg += Pntr[i];
     }
 
@@ -243,7 +226,7 @@ void InitStoreblocksizes()
     Store_block_sizes.resize(Target_type_len);
 
     Store_block_sizes[Target_type_len - 1] = 1;
-    for (int i=Target_type_len - 2; i>=0; i--) {
+    for (int i = Target_type_len - 2; i >= 0; i--) {
         Store_block_sizes[i] = (Target_type[i+1] + 1) * Store_block_sizes[i+1]; 
     }
 }
@@ -263,7 +246,7 @@ void InitStoreblocksizes()
 /* FUNCTION:                                                       */
 /*     Count all subtypes of the given Target_type.                */
 /*******************************************************************/ 
-void FillTypecount(int Cur_scan_pos)
+void FillTypecount(vector<int> &Type_count, int Cur_scan_pos)
 {
     if (Cur_scan_pos > Target_type_len) {    
         int d = 0;
@@ -272,10 +255,10 @@ void FillTypecount(int Cur_scan_pos)
         }
         Type_count[d]++;
     } else {
-        for (int i=0;i<=Target_type[Cur_scan_pos];i++) {
+        for (int i = 0; i <= Target_type[Cur_scan_pos]; i++) {
             int save = Target_type[Cur_scan_pos];
             Target_type[Cur_scan_pos] = i;
-            FillTypecount(Cur_scan_pos + 1);
+            FillTypecount(Type_count, Cur_scan_pos + 1);
             Target_type[Cur_scan_pos] = save;
         }
     }
@@ -298,35 +281,32 @@ void FillTypecount(int Cur_scan_pos)
 /*     Initialize Type_table_index[] -- Type to Type_table.        */ 
 /*     Initialize Deg_to_type_table_index[] -- Degree to Type.     */ 
 /*******************************************************************/ 
-int InitTypetable()
+int InitTypetable(const vector<int> &Type_count)
 {
     int i;
 
     Tot_subtypes = 0;
 
-    for (i=0;i<=Target_type_deg;i++)
+    for (i = 0; i <= Target_type_deg; i++) {
         Tot_subtypes += Type_count[i];
+    }
 
-    Type_table = (TT_node *) Mymalloc(Tot_subtypes * sizeof(TT_node));
-    assert_not_null(Type_table);
+    Type_table.resize(Tot_subtypes);
 
-    for (i=0;i<Tot_subtypes;i++) {
+    for (i = 0; i < Tot_subtypes; i++) {
         Type_table[i].type = GetNewType(); 
-        assert_not_null(Type_table[i].type);
-        Type_table[i].begin_basis = Type_table[i].end_basis = 0;
+        Type_table[i].begin_basis = 0;
+        Type_table[i].end_basis = 0;
     }
    
-    Deg_to_type_table_index = (int *) Mymalloc((Target_type_deg + 1) * sizeof(int));
-    assert_not_null(Deg_to_type_table_index);
-
+    Deg_to_type_table_index.resize(Target_type_deg + 1);
     Deg_to_type_table_index[0] = 0;
     for (i = 1; i <= Target_type_deg; i++) {
         Deg_to_type_table_index[i] = Deg_to_type_table_index[i - 1] + Type_count[i - 1];
     }
 
-    Type_table_index = (int *) Mymalloc(Tot_subtypes * sizeof(int));
-    assert_not_null(Type_table_index);
-    { 
+    Type_table_index.resize(Tot_subtypes);
+    {
       vector<int> Temp_dttt_index(Target_type_deg + 1);         /* Used to fill Type_table_index.  */
 
       for (i=0; i<=Target_type_deg; i++) {
@@ -342,24 +322,13 @@ int InitTypetable()
 
 void DestroyTypeTable()
 {
-    if(Type_table) {
-      for (int i = 0; i < Tot_subtypes; i++) {
-        if(Type_table[i].type) free(Type_table[i].type);
-      }
-
-      free(Type_table);
-      Type_table = nullptr;
+    for(auto & i : Type_table) {
+        free(i.type);
     }
+    Type_table.clear();
 
-    if(Deg_to_type_table_index) {
-      free(Deg_to_type_table_index);
-      Deg_to_type_table_index = nullptr;
-    }
-
-    if(Type_table_index) {
-      free(Type_table_index);
-      Type_table_index = nullptr;
-    }
+    Deg_to_type_table_index.clear();
+    Type_table_index.clear();
 
     Store_block_sizes.clear();
 }
@@ -441,7 +410,7 @@ int GetIndex(const Type Pntr)
 Name FirstTypeDegree(Degree D)
 {
     if ((D >= 0) && (D <= Target_type_deg)) {
-        return (Deg_to_type_table_index[(int) D]);
+        return Deg_to_type_table_index[(int) D];
     }
 
     return -1;
@@ -563,67 +532,25 @@ void PrintTypetableindex()
 #endif
 
 bool save_type_table(FILE *f) {
-//    typedef char Degree;
-//    typedef Degree *Type;
-//    typedef struct tt_node {
-//        Basis begin_basis;
-//        Basis end_basis;            /* indices into Base table. */
-//        Type type;
-//    } TT_node;
-//    static Type Target_type = nullptr;             /* Input from higher level module. */
-//    static int Target_type_len = 0;                /* Computed from Target_type.      */
-//    static int Target_type_deg = 0;                /* Computed from Target_type.      */
-//    static int *Type_count = nullptr;              /* Used to fill Type_table.        */
-//    static TT_node *Type_table = nullptr;          /* Heart of the matter.            */
-//    static int *Type_table_index = nullptr;        /* Map type to Type_table.         */
-//    static int Tot_subtypes = 0;                   /* Computed from Type_count. Size of Type_table. */
-//    static int *Deg_to_type_table_index = nullptr; /* Map Degree to Type_table.*/
-//    static vector<int> Store_block_sizes;          /* To find offset into Type_table. */
-
-//    {
-//        int n = Basis_table.size();
-//        fwrite(&n, sizeof(n), 1, f);
-//        for(int i=0; i<n; i++) {
-//            auto a = Basis_table[i];
-//            fwrite(&a, sizeof(a), 1, f);
-//        }
-//    }
-//    {
-//        int n = Deg_to_basis_table.size();
-//        fwrite(&n, sizeof(n), 1, f);
-//        for(int i=0; i<n; i++) {
-//            auto a = Deg_to_basis_table[i].first;
-//            auto b = Deg_to_basis_table[i].second;
-//            fwrite(&a, sizeof(a), 1, f);
-//            fwrite(&b, sizeof(b), 1, f);
-//        }
-//    }
+    int n = last_type.size();
+    fwrite(&n, sizeof(n), 1, f);
+    if(n > 0) {
+        fwrite(&last_type[0], sizeof(last_type[0]), n, f);
+    }
 
     return true;
 }
 
 bool restore_type_table(FILE *f) {
-//    Basis_table.clear();
-//    {
-//        int n = 0;
-//        if(fread(&n, sizeof(n), 1, f) != 1) return false;
-//        Basis_table.resize(n);
-//        for(int i=0; i<n; i++) {
-//            if(fread(&Basis_table[i], sizeof(Basis_table[i]), 1, f) != 1) return false;
-//        }
-//    }
-//    Deg_to_basis_table.clear();
-//    {
-//        int n = 0;
-//        if(fread(&n, sizeof(n), 1, f) != 1) return false;
-//        Deg_to_basis_table.resize(n);
-//        for(int i=0; i<n; i++) {
-//            Basis a, b;
-//            if(fread(&a, sizeof(a), 1, f) != 1) return false;
-//            if(fread(&b, sizeof(b), 1, f) != 1) return false;
-//            Deg_to_basis_table[i] = make_pair(a, b);
-//        }
-//    }
+    vector<Degree> tmp;
+    {
+        int n = 0;
+        if(fread(&n, sizeof(n), 1, f) != 1) return false;
+        if (n > 0) {
+            tmp.resize(n);
+            if (fread(&tmp[0], sizeof(tmp[0]), n, f) != n) return false;
+        }
+    }
 
-    return true;
+    return CreateTypeTable(&tmp[0]) == OK;
 }
