764bdc8197ff90187be4937d438dafde134d7d72
Kent VanderVelden <kent.vandervelden@gmail.com>
2020-12-26 19:35:16 -0600

    Factored out time path

--- profiled_make.sh
+++ profiled_make.sh
@@ -5,7 +5,9 @@ set -e
 make clean
 make
 
-/usr/bin/time ./albert < tests/profile.in
+TIME=/usr/bin/time
+
+$TIME ./albert < tests/profile.in
 rm basis.txt mult.txt mult.bin
 
 CFLAGS="-g -O3 -Wall -fopenmp -fprofile-correction -mavx"
@@ -15,13 +17,13 @@ rm -f *.gcda
 make clean
 make CFLAGS="$CFLAGS -fprofile-generate" LDFLAGS="$LDFLAGS -fprofile-generate"
 
-/usr/bin/time ./albert < tests/profile.in
+$TIME ./albert < tests/profile.in
 rm basis.txt mult.txt mult.bin
 
 make clean
 make CFLAGS="$CFLAGS -fprofile-use" LDFLAGS="$LDFLAGS -fprofile-use"
 rm -f *.gcda
 
-/usr/bin/time ./albert < tests/profile.in
+$TIME ./albert < tests/profile.in
 rm basis.txt mult.txt mult.bin
 
